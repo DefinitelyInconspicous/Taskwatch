539465092f70a8b763add0bf6468de941ab645c2
Avyan Mehra <avyan_mehra@s2023.ssts.edu.sg>
2024-01-09 15:11:08 +0800

    Initial Commit

---
+++ Taskwatch/TaskwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskwatchApp.swift
+//  Taskwatch
+//
+//  Created by Avyan Mehra on 9/1/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
