1e2d78f64a05315c97e10a76c651ce35b7b1c46b
Avyan Mehra <avyan_mehra@s2023.ssts.edu.sg>
2024-01-10 17:31:46 +0800

    Fixed bug and added TaskView for details on task.

---
+++ TaskView.swift
@@ -0,0 +1,18 @@
+//
+//  TaskView.swift
+//  Taskwatch
+//
+//  Created by Avyan Mehra on 10/1/24.
+//
+
+import SwiftUI
+
+struct TaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskView()
+}
