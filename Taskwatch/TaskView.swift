2d01b3feaa6fe680bd592d690fab053429ee00bc
Avyan Mehra <avyan_mehra@s2023.ssts.edu.sg>
2024-01-11 15:30:43 +0800

    Added stopwatch template

--- Taskwatch/TaskView.swift
+++ Taskwatch/TaskView.swift
@@ -8,13 +8,33 @@
 import SwiftUI
 
 struct TaskView: View {
+    @State var taskHour: Int = 0
+    @State var taskMin: Int = 0
     @Binding var taskName: String
     @Binding var taskDesc: String
+    @Binding var taskTime: Int
     var body: some View {
         NavigationStack {
             
             Text(taskDesc)
                 .font(.title3)
+            .padding()
+            ZStack {
+                Rectangle()
+                    .fill(.gray)
+                    .frame(width: 350, height: 100)
+                    .cornerRadius(10)
+                HStack {
+                    Circle()
+                        .fill(.red)
+                        .frame(width: 60, height: 60)
+                    Circle()
+                        .fill(.blue)
+                        .frame(width: 60, height: 60)
+                }
+                
+            }
+            
             
             
             .navigationTitle(taskName)
